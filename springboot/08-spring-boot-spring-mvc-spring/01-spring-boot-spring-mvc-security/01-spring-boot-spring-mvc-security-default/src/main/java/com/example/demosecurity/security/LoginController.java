06235695ee8958b7be580ffd5ed6a1a4fc7341e0
0noor <91561134+0noor@users.noreply.github.com>
2025-06-09 18:21:53 -0400

    -9

---
+++ springboot/08-spring-boot-spring-mvc-spring/01-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/example/demosecurity/security/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.demosecurity.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLogInPage")
+	private String showMyLogInPage() {
+		
+		return "fancy-login";
+	}
+	
+}
